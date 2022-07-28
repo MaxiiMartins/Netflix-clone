28631068a100ab667e8b592487cba507d5a2d547
Maximiliano <maximartins45@gmail.com>
2022-07-28 01:43:59 -0300

    Create LandingPage.jsx

---
+++ src/pages/Landing/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import "./LandingPage.scss"
+
+function LandingPage() {
+  return (
+    <div>
+        Bienvenido!!!
+        <Link to="/page/">Ingresarrrrrrrrrrr</Link>
+    </div>
+  )
+}
+
+export default LandingPage
