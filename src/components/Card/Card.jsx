3db9cf41072b984b4a94392c303d869791ed094a
Maximiliano <maximartins45@gmail.com>
2022-07-28 01:43:11 -0300

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import "./Card.scss"
-
-function Card({imgUrl}) {
-  return (
-    <img className='card' src={imgUrl} alt="cover" />
+import { Link } from "react-router-dom";
+import {motion} from "framer-motion"
+ 
+function Card({imgUrl="https://png.pngtree.com/png-vector/20201224/ourlarge/pngtree-error-404-page-not-found-png-image_2598541.jpg",id}) {
+  return (<>
+    <Link to={`/page/detalle/${id}`}>
+    <motion.img whileHover={{transform:{traslate:"-10px"}}} className='card' draggable="false" src={imgUrl} alt="cover" />
+    </Link>
+  </>
   )
 }
 
