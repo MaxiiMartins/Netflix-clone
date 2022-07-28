29996868180202ee7ddf68c99f111145f981531e
Maximiliano <maximartins45@gmail.com>
2022-07-28 01:43:20 -0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import "./Header.scss"
-import logo from "../../assets/logo.png"
-import { Link } from "react-router-dom";
+import logo from "../../assets/logo.webp"
+import { Link ,Outlet} from "react-router-dom";
 import {ImSearch} from "react-icons/im"
 
 function Header() {
-  return <nav className="header">
-    <img src={logo} alt="logo" />
+  return <><nav className="header">
+        <Link to="/page/home"><img src={logo} draggable="false" alt="logo" /></Link>
+        
     <div>
-      <Link to="/tvshows">Series</Link>
-      <Link to="/movies">Peliculas</Link>
-      <Link to="/recent">Añadidos Recientemente</Link>
-      <Link to="/mylist">Mi lista</Link>
+      <Link to="/page/series">Series</Link>
+      <Link to="/page/peliculas">Peliculas</Link>
+      {/* <Link to="/page/recent">Añadidos Recientemente</Link> */}
+      <Link to="/page/mylist">Mi lista</Link>
     </div>
     {/* Va por el minuto 22:53 del vidio */}
     <ImSearch/>
-  </nav>;
+  </nav>
+  <Outlet/>
+  </>;
 }
 
 export default Header;
