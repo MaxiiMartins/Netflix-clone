5f98fece417cc3b3e0917ca87088b8864f93e84f
Maximiliano <maximartins45@gmail.com>
2022-08-01 00:46:44 -0300

    Update Row.jsx

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Card from "../Card/Card";
 import "./Row.scss";
 
-function Row({ title, arr = [] }) {
+function Row({ title, arr = [],cambiar,cargar}) {
   const URL_IMG = "https://image.tmdb.org/t/p/w500";
   // &query=whiplash&language=es-AR&region=AR
   return (
     <div className="row-card">
       <h2>{title}</h2>
       <div>
-        {arr.filter(item=> item.poster_path || item.backdrop_path).map((items,index)=>(<Card key={index} id={items.id} imgUrl={`${URL_IMG}${items.poster_path || items.backdrop_path}`}/>))}
+        {arr.filter(item=> item.poster_path || item.backdrop_path).map((items,index)=>(<Card key={index} id={items.id} imgUrl={`${URL_IMG}${items.poster_path || items.backdrop_path}`} cargarinfo={()=>{cargar(items);cambiar()}}/>))}
         {/* {arr.filter(item=> item.poster_path || item.backdrop_path).map((items,index)=>(<Link to={`/detalle/${items.id}`}><Card key={index} imgUrl={`${URL_IMG}${items.poster_path || items.backdrop_path}`}/></Link>))} */}
       </div>
     </div>
